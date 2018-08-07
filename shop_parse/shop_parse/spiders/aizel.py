85144ad1246c28f51b0814c7c4520368c788d0e9
KayafasKain <bismark9988@gmail.com>
2018-08-07 14:26:22 +0300

    better json output , data passing changed

--- shop_parse/shop_parse/spiders/aizel.py
+++ shop_parse/shop_parse/spiders/aizel.py
@@ -12,50 +12,50 @@ class AizelSpider(scrapy.Spider):
         for href in response.xpath('//li[contains(@class, "product__item")]//a/@href').extract():
             if requests < 1:
                 link = 'https://aizel.ru' + href
-                yield scrapy.Request(link, callback=self.parse_sizes, meta={'link': link})
+                yield scrapy.Request(link, callback=self.parse_category, meta={'link': link})
             else:
                 break
             requests += 1
 
-    def parse_sizes(self, response):
+    def parse_category(self, response):
         link = response.meta.get('link')
         id = re.search('[0-9]+', link)
         link = 'https://aizel.ru/products/sizes/?id=' + id.group(0)
-        yield scrapy.Request(link, callback=self.parse_category, meta={'page_response': response})
-
-    def parse_category(self, response):
         item = ShopParseItem()
-        page_response = response.meta.get('page_response')
-        item['ware_name'] = self.parse_ware_name(page_response)
-        item['price'] = self.parse_price(page_response)
-        item['currency'] = self.parse_currency(page_response)
-        item['description'] = self.parse_description(page_response)
-        item['images'] = self.parse_images(page_response)
-        item['brand'] = self.parse_brand(page_response)
+        item['ware_name'] = self.parse_ware_name(response)
+        item['price'] = self.parse_price(response)
+        item['currency'] = self.parse_currency(response)
+        item['description'] = self.parse_description(response)
+        item['images'] = self.parse_images(response)
+        item['brand'] = self.parse_brand(response)
+        yield scrapy.Request(link, callback=self.parse_sizes, meta={'item': item})
+
+    def parse_sizes(self, response):
+        item = response.meta.get('item')
         item['sizes'] = self.parse_clth_size(response)
         yield item
 
     def parse_ware_name(self, response):
         return response.selector.xpath(
             '//div[contains(@class, "product-item__header")]/h1[contains(@class, "product-item__name-desc")]/@content'
-            ).extract()
+            ).extract_first()
 
     def parse_price(self, response):
         return response.selector.xpath(
             '//div[contains(@class, "product-item__header")]//div[contains(@class, "product__desc__price product__desc__price_int")]' +
             '/span/text()'
-        ).extract()
+        ).extract_first()
 
     def parse_currency(self, response):
         return response.selector.xpath(
             '//div[contains(@class, "product-item__header")]//div[contains(@class, "product__desc__price product__desc__price_int")]' +
             '/span/@data-symbol'
-        ).extract()
+        ).extract_first()
 
     def parse_description(self, response):
         return response.selector.xpath(
             '//div[contains(@class, "accordion__item__content")]/p/text()'
-        ).extract()
+        ).extract_first()
 
     def parse_images(self, response):
         return response.selector.xpath(
@@ -65,7 +65,7 @@ class AizelSpider(scrapy.Spider):
     def parse_brand(self, response):
         return response.selector.xpath(
             '//div[contains(@class, "product-item__header")]/a[contains(@class, "product-item__name")]/text()'
-            ).extract()
+            ).extract_first()
 
     def parse_clth_size(self, response):
         return response.selector.xpath(
